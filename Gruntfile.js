ab028eab54152bd5d47cf22b9af6083b21c1c2f1
Tomasz Dysinski <oz@bunnybones.com>
2013-11-28 01:28:42 -0500

    cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,17 @@
 module.exports = function(grunt) {
 	
-	var FS_PATH = require('path');
-	
-	var TEXTUREPACKER_PATH = "";
-	
 	function log(err, stdout, stderr, cb) {
 	    console.log(stdout);
 	    cb();
 	}
 	
 	grunt.initConfig({
-		less: {
-			development: {
-				options: {
-					paths: ['html/styles']
-				},
-				src: 'html/styles/main.less',
-				dest: 'html/styles/main.css'
-			}
-		},		
 	    shell: {
-	        mongo: {
-	            command: [
-	                'mkdir database/db',
-	                '"c:\\Program Files\\mongodb\\bin\\mongod.exe" --dbpath "c:\\Tomasz\\repos\\QuadPaintJS\\database"'
-	            ].join('&&'),
-		        options: {
-		            //async: true,
-		            stdout: true,
-		            stderr: true,
-		            callback: log
-		        }
-	        },
 		    beefy: {
 		      command: ['beefy',
 		      	'scripts/main.js',
 		      	'--cwd html'].join(" ")
-		    },
-			TexturePacker: {
-                options: {
-                	stdout: true,
-					strderr: true,
-					async: false
-                },
-                command: ["TexturePacker --data debug/styles/sprite.less",
-				" --sheet debug/assets/ui/sprite.png",
-				" --texturepath debug/assets/ui/",
-				" --texture-format png",
-				" --algorithm Basic",
-				" --trim-mode None",
-				" --force-publish",
-				" --format multi-css",
-				" --size-constraints POT",
-				" design/icons"].join(" ")
-			}
+		    }
 		},
 		concurrent: {
 			dev: {
@@ -64,11 +22,7 @@ module.exports = function(grunt) {
 		}
 	});
 	grunt.loadNpmTasks('grunt-concurrent');
-	//grunt.loadNpmTasks('grunt-shell');
 	grunt.loadNpmTasks('grunt-shell-spawn');
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	
 	grunt.registerTask('default', ['concurrent']);
-	grunt.registerTask('TexturePacker', ['shell:TexturePacker'] );
 };
--- html/scripts/main.js
+++ html/scripts/main.js
@@ -1,26 +1,7 @@
-var url = require('url'),
-http 		= require('http'),
-director 	= require('director'),
-screenfull 	= require('./vendor/screenfull'),
-Global 		= require('./Global'),
+var Global 		= require('./Global'),
 QuadPaint   = require('./QuadPaint');
 
 $('document').ready(function(){
     
 	var quadPaint = new QuadPaint();
-	
-	var routes = {
-		'/startup': {
-			on: function(){
-
-			}		
-		},
-		'/:painting': {
-			on: function(section){
-				Nav.fadeIn(section);				
-			}
-		}
-	 };
-
-	var router = new director.Router(routes);
 });
